e39a6ea32daa0ea060b5d68fa2abd92d538ef978
Bouchedoub Ramzi <ios191169@gmail.com>
2023-02-12 11:19:21 +0100

    Initial Commit

---
+++ findingWater/findingWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  findingWaterApp.swift
+//  findingWater
+//
+//  Created by Bouchedoub Ramzi on 12/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct findingWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
