b20b7c298858411f255c7089db4dc2dad18d3a94
Ramzi Bouchedoub <ramzibouchedoub@Ramzis-MacBook-Air.local>
2023-04-08 14:19:04 +0100

    refactor: sources model

--- findingWater/sources.swift
+++ findingWater/sources.swift
@@ -6,34 +6,31 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestoreSwift
 import CoreLocation
 
-struct Sources2: Identifiable, Codable {
-    var id: String?
+struct Sources: Identifiable ,Codable {
+    @DocumentID var id: String?
     var name: String
-
-   // let address: String
-   // let location: CLLocationCoordinate2D
-   // let image: String
+    var adresser:String
+    var location: GeoPoint
+    //var image:String
     
-   /* enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case address
-       // case latitude
-       // case longitude
-        case image
-    }*/
+    enum CodingKeys: String, CodingKey {
+      case id
+      case name
+      case adresser
+      case location
+      //case latitude
+     // case longitude
+      //case image
+      
+    }
     
-    /*init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decodeIfPresent(String.self, forKey: .id)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.address = try container.decode(String.self, forKey: .address)
-       // let latitude = try container.decode(Double.self, forKey: .latitude)
-       // let longitude = try container.decode(Double.self, forKey: .longitude)
-        //self.location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        self.image = try container.decode(String.self, forKey: .image)
-    }*/
+   
+    
 }
+
+
 
