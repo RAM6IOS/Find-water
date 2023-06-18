b1b569c50c71901031280a363b2510c2a83d7858
RAM6IOS <ios191169@gmail.com>
2023-06-18 22:49:28 +0100

    refactor

--- findingWater/View/locationDitels.swift
+++ findingWater/View/locationDitels.swift
@@ -25,104 +25,122 @@ struct locationDitels: View {
     @ObservedObject var viewModel2 = CreateAccount()
     @ObservedObject var mapViewModel = LocationMapViewModel()
     var body: some View {
-        VStack{
-            Text(soures.name)
-            Text("latitude:\(soures.location.latitude)")
-            Text("latitude:\(soures.location.longitude)")
-            Button{
-                openMap(coordinate: CLLocationCoordinate2D(latitude: soures.location.latitude, longitude: soures.location.longitude))
-            }label: {
+        VStack(alignment: .leading){
+            Image("caption")
+                .resizable()
+                           .scaledToFit()
+                           .aspectRatio(contentMode: .fill)
+                           .frame(minWidth: nil, idealWidth: nil, maxWidth: UIScreen.main.bounds.width, minHeight: nil, idealHeight: nil, maxHeight: 300, alignment: .center)
+                           //.clipped()
+            VStack(alignment: .leading,spacing: 10){
+                Text(soures.name)
+                    .font(.title)
                 HStack{
-                    Image("send")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 25, height: 25)
-                    Text("Direction")
+                    Image(systemName: "mappin.and.ellipse")
+                    Text("Algeria ,Blida ,ouled slama")
+                        
                 }
-                .foregroundColor(.white)
-                .frame(width: 200 ,height: 50)
-                .background(Color.blue)
+                .font(.title3)
+                Text("Coordinate:")
+                    .font(.title3)
+                Text("latitude:\(soures.location.latitude)")
+                Text("latitude:\(soures.location.longitude)")
                 
-            }
-            .cornerRadius(10)
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                   
-                    
-                    
-                    Button{
-                        
-                        if viewModel2.userSession != nil  {
-                            viewModel2.currentUser?.value.toggle()
-                            
-                            let docData: [String: Any] = [
-                                // "uid": uid,
-                                "name": "PoutineFiesta",
-                                "address": "1234 Restaurant St"
-                            ]
-                            let db = Firestore.firestore()
-                            
-                            let docRef = db.collection(soures.name).document()
-                            
-                            docRef.setData(docData) { error in
-                                if let error = error {
-                                    print("Error writing document: \(error)")
-                                } else {
-                                    print("Document successfully written!")
-                                }
-                            }
-                            
-                            viewModel2.EditProducti(id: viewModel2.currentUser?.id  ?? "reewwedsewd", value: viewModel2.currentUser?.value ?? true)
-                        }
-                        
-                    } label: {
-                        Image(systemName: "plus")
-                        // .foregroundColor(.white)
-                            .padding()
-                        //.background(.green)
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                Button{
+                    openMap(coordinate: CLLocationCoordinate2D(latitude: soures.location.latitude, longitude: soures.location.longitude))
+                }label: {
+                    HStack{
+                        Text("Get Directions")
+                           
+                        Image(systemName: "arrow.forward")
                     }
-                    .disabled( viewModel2.currentUser?.value == true)
+                    .foregroundColor(.white)
+                    .frame(width: 350 ,height: 50)
+                    .background(Color.blue)
                     
-                    
-                    ForEach(mapViewModel.user, id: \.self) { item in
+                }
+                .cornerRadius(25)
+            }
+            .padding()
+            VStack{
+                Text("People on Site:")
+                    .font(.title3)
+                    .bold()
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
                         Button{
-                            if viewModel2.userSession != nil &&   viewModel2.currentUser?.value == true {
+                            
+                            if viewModel2.userSession != nil  {
                                 viewModel2.currentUser?.value.toggle()
                                 
-                                Firestore.firestore().collection(soures.name).document(item.id ?? "eereded").delete() { err in
-                                    if let err = err {
-                                        print("Error removing document: \(err)")
+                                let docData: [String: Any] = [
+                                    // "uid": uid,
+                                    "name": "PoutineFiesta",
+                                    "address": "1234 Restaurant St"
+                                ]
+                                let db = Firestore.firestore()
+                                
+                                let docRef = db.collection(soures.name).document()
+                                
+                                docRef.setData(docData) { error in
+                                    if let error = error {
+                                        print("Error writing document: \(error)")
                                     } else {
-                                        print("Document successfully removed!")
+                                        print("Document successfully written!")
                                     }
                                 }
                                 
-                                viewModel2.EditProducti(id: viewModel2.currentUser?.id  ?? "reewwedsewd", value:  viewModel2.currentUser?.value ?? false )
-                                
+                                viewModel2.EditProducti(id: viewModel2.currentUser?.id  ?? "reewwedsewd", value: viewModel2.currentUser?.value ?? true)
                             }
                             
                         } label: {
-                            Image(systemName: "person.fill")
-                                .foregroundColor(.white)
+                            Image(systemName: "plus")
+                            // .foregroundColor(.white)
                                 .padding()
-                                .background(.green)
+                            //.background(.green)
                                 .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.black, lineWidth: 1))
                         }
+                        .disabled( viewModel2.currentUser?.value == true)
+                        ForEach(mapViewModel.user, id: \.self) { item in
+                            Button{
+                                if viewModel2.userSession != nil &&   viewModel2.currentUser?.value == true {
+                                    viewModel2.currentUser?.value.toggle()
+                                    
+                                    Firestore.firestore().collection(soures.name).document(item.id ?? "eereded").delete() { err in
+                                        if let err = err {
+                                            print("Error removing document: \(err)")
+                                        } else {
+                                            print("Document successfully removed!")
+                                        }
+                                    }
+                                    
+                                    viewModel2.EditProducti(id: viewModel2.currentUser?.id  ?? "reewwedsewd", value:  viewModel2.currentUser?.value ?? false )
+                                    
+                                }
+                                
+                            } label: {
+                                Image(systemName: "person.fill")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(.green)
+                                    .clipShape(Circle())
+                            }
+                            
+                        }
+                    }
+                    .onAppear{
+                        mapViewModel.fetchData2(mdel:soures.name)
+                        self.viewModel2.fetchUser()
+                        //.viewModel2.userSession
                         
                     }
-                    
-                    
-                }
-                .onAppear{
-                    mapViewModel.fetchData2(mdel:soures.name)
-                    self.viewModel2.fetchUser()
-                    viewModel2.userSession
-                    
                 }
             }
+            .padding()
         }
+        
+        .navigationTitle(soures.name)
     }
     func openMap(coordinate:CLLocationCoordinate2D){
         let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate))
