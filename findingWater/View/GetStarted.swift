aed049bfd97fa59179fa619822614675973ee6d2
RAM6IOS <ios191169@gmail.com>
2023-07-06 12:20:10 +0100

    refactor

--- findingWater/View/GetStarted.swift
+++ findingWater/View/GetStarted.swift
@@ -9,57 +9,99 @@ import SwiftUI
 
 struct GetStarted: View {
     @Binding var show: Bool
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     var body: some View {
         GeometryReader { reader in
             VStack {
                 Spacer()
-                Image("map")
+                Image("maps-and-flags")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: reader.size.width * (2/5), height: reader.size.height * (2/5))
+                    .frame(width: reader.size.width * (1.5/5), height: reader.size.height * (1.5/5))
                     .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
                     .padding(.top, 44)
                 VStack {
-                    Text("Welcome")
-                        .font(.system(size: 40))
-                        .bold()
-                    if #available(iOS 16.0, *) {
-                        Text("finding Water")
-                            .font(.system(size: 24))
-                            .minimumScaleFactor(0.5)
-                            .fontWeight(.bold)
-                    } else {
-                        // Fallback on earlier versions
-                    }
-                    HStack(alignment: .center) {
-                        Image("location")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 100)
-                            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                        VStack {
-                            Text("Resource water Locations").bold()
-                            Text("Find places to resource water")
-                                .foregroundColor(.secondary)
-                                .lineLimit(2)
-                                .minimumScaleFactor(0.75)
+                    if UIDevice.current.userInterfaceIdiom == .pad {
+                        Text("Welcome")
+                            .font(.system(size: 60))
+                        if #available(iOS 16.0, *) {
+                            Text("finding Water")
+                                .font(.system(size: 40))
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        } else {
+                            // Fallback on earlier versions
                         }
-                    }
-                }
-                .padding()
-                Spacer()
-                Button {
-                    show.toggle()
-                }label: {
-                    Text("GetStarted")
-                        .font(.headline)
-                        .foregroundColor(.white)
+                        HStack(alignment: .center,spacing: 40) {
+                            Image("route")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: reader.size.width * (1/5), height: reader.size.height * (1/5))
+                                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                            VStack {
+                                Text("Resource water Locations").bold()
+                                Text("Find places to resource water")
+                                    .foregroundColor(.white)
+                            }
+                            .font(.system(size: 40))
+                            .minimumScaleFactor(0.5)
+                        }
+                        Spacer()
+                        Button {
+                            show.toggle()
+                        }label: {
+                            Text("Get Started")
+                                .font(.largeTitle)
+                                .foregroundColor(.blue)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                        }
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                    } else {
+                        Text("Welcome")
+                            .font(.title)
+                            .minimumScaleFactor(0.5)
+                        if #available(iOS 16.0, *) {
+                            Text("finding Water")
+                                .font(.title2)
+                                .minimumScaleFactor(0.5)
+                                .fontWeight(.bold)
+                        } else {
+                            // Fallback on earlier versions
+                        }
+                        HStack(alignment: .center) {
+                            Image("location")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: reader.size.width * (1/5), height:  reader.size.height * (1/5))
+                                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                            VStack {
+                                Text("Resource water Locations").bold()
+                                Text("Find places to resource water")
+                                    .foregroundColor(.secondary)
+                            }
+                            .font(.title3)
+                            .minimumScaleFactor(0.5)
+                        }
+                        Spacer()
+                        Button {
+                            show.toggle()
+                        }label: {
+                            Text("Get Started")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        .padding()
+                    }
                 }
                 .padding()
             } }
+        .background(.blue)
     }
 }
