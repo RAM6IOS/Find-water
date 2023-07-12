f4d499049dfb28b7ce7aedb3b0a6c1fafbd1ff86
RAM6IOS <ios191169@gmail.com>
2023-07-12 20:23:09 +0100

    refactor: location map view

--- findingWater/View model/UpdateEmailViewModel.swift
+++ findingWater/View model/UpdateEmailViewModel.swift
@@ -8,8 +8,47 @@
 import Foundation
 import Firebase
 import FirebaseAuth
+import Combine
 class  UpdateEmailViewModel: ObservableObject {
-    func updateEmail(newEmail: String, password: String, id:String, email:String) {
+    private var cancellables = Set<AnyCancellable>()
+        
+        func updateEmail(newEmail: String, password: String, id: String, email: String) {
+            guard let user = Auth.auth().currentUser else {
+                print("No authenticated user")
+                return
+            }
+            
+            // Prompt the user to reauthenticate
+            let credential = EmailAuthProvider.credential(withEmail: user.email ?? "", password: password)
+            
+            user.reauthenticate(with: credential)
+                .flatMap { _ in
+                    user.updateEmail(to: newEmail)
+                        .eraseToAnyPublisher()
+                }
+                .receive(on: DispatchQueue.main)
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        // Handle the update email error
+                        print("Error updating email: \(error.localizedDescription)")
+                    case .finished:
+                        // Email updated successfully
+                        print("Email updated successfully")
+                        self.editProducti(id: id, email: email)
+                    }
+                } receiveValue: { _ in }
+                .store(in: &self.cancellables)
+        }
+
+        
+        
+        
+    
+   
+
+    /*
+    func updateEmail(newEmail: String, password: String, id: String, email: String) {
         let user = Auth.auth().currentUser
         // Prompt the user to reauthenticate
         let credential = EmailAuthProvider.credential(withEmail: user?.email ?? "medsmewd,rewd", password:password)
@@ -32,6 +71,7 @@ class  UpdateEmailViewModel: ObservableObject {
             }
         }
     }
+     */
     func editProducti(id: String, email: String) {
         Firestore.firestore().collection("users").document(id)
             .updateData(["email": email
